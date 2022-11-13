bb92399613b8945c4c48389d4c0489163eecd3ca
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-13 14:56:33 +0200

    feat(footer and social media list): footer done

--- backend/landing/core/library/organisms/Footer/Footer.js
+++ backend/landing/core/library/organisms/Footer/Footer.js
@@ -4,40 +4,33 @@ import { useId } from "react";
 import styles from "@/styles/_05_library/organisms/footer/footer.module.scss";
 
 import Copyright from "@/molecules/Copyright/Copyright";
-import LinkIcon from "@/atoms/LinkIcon/LinkIcon";
+import SocialMediaList from "@/molecules/SocialMediaList/SocialMediaList";
 
 export default function Footer() {
   const ID = useId();
 
+  const socialMedia = [
+    {
+      name: "twitter",
+      href: "http://twitter.com",
+      label: "Twitter",
+    },
+    {
+      name: "facebook",
+      href: "http://facebook.com",
+      label: "Facebook",
+    },
+    {
+      name: "linkedin",
+      href: "http://linkedin.com",
+      label: "LinkedIn",
+    },
+  ];
+
   return (
     <footer className={styles.base} id="footer" data-next={`${config.name}-${ID}`}>
-      <LinkIcon />
+      <SocialMediaList list={socialMedia} />
       <Copyright />
-      {/* <ul className={styles.iconList}>
-        <li className={styles.iconListItem}>
-          <a href="#" className="icon brands alt fa-twitter">
-            <SlSocialTwitter className={styles.icon}/>
-          </a>
-        </li>
-        <li className={styles.iconListItem}>
-          <a href="#" className="icon brands alt fa-facebook-f">
-            <SlSocialFacebook className={styles.icon}/>
-          </a>
-        </li>
-        <li className={styles.iconListItem}>
-          <a href="#" className="icon brands alt fa-linkedin-in">
-            <SlSocialLinkedin className={styles.icon}/>
-          </a>
-        </li>
-        <li className={styles.iconListItem}>
-          <a href="#" className="icon brands alt fa-linkedin-in">
-            <SlEnvolope className={styles.icon}/>
-          </a>
-        </li>
-      </ul>
-      <div className={styles.copyrightContainer}>
-        <p className={styles.copyright}>{date.getFullYear()} &copy; LynxIT</p>
-      </div> */}
     </footer>
   );
 }
