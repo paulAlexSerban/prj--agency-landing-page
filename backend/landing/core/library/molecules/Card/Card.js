b92cb272fec06755550392567953c9ba83b2847d
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-17 18:30:13 +0200

    refactor(molecules): restructure molecules

--- backend/landing/core/library/molecules/Card/Card.js
+++ backend/landing/core/library/molecules/Card/Card.js
@@ -1,10 +1,10 @@
 import { useId } from "react";
-import { config } from "./config";
-import styles from "@/styles/_05_library/molecules/card/card.module.scss";
+import config from "./config";
+import styles from "@/styles/molecules/card/card.module.scss";
 
-import Paragraph from "@/atoms/Paragraph/Paragraph";
-import Icon from "@/atoms/Icon/Icon";
-import Heading from "@/atoms/Heading/Heading";
+import Paragraph from "@/core/atoms/Paragraph/Paragraph";
+import Icon from "@/core/atoms/Icon/Icon";
+import Heading from "@/core/atoms/Heading/Heading";
 
 export default function Card({iconName, heading, text}) {
   const ID = useId();
