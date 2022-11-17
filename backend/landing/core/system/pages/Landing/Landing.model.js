86688c4c47fdcfa5e1ff0c8149bb23a11bf81a1a
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-17 15:46:04 +0200

    refactor: refactor pages

--- backend/landing/core/system/pages/Landing/Landing.model.js
+++ backend/landing/core/system/pages/Landing/Landing.model.js
@@ -1,7 +1,53 @@
+/**
+ * content from ./content/landing and ./content/fragments should be rendered here
+ */
+
 export default async function getStaticProps() {
   return {
     props: {
-      type: "Landing",
+      pageProperties: {
+        templateType: ""
+      },
+      content: {
+        main: {}
+      },
     },
   };
 }
+
+
+const navList = [
+  { href: "despre_noi", label: "despre noi" },
+  { href: "servicii", label: "servicii" },
+  { href: "produse", label: "produse" },
+  { href: "beneficii", label: "beneficii" },
+  { href: "contact", label: "contact" },
+];
+
+const spotlight_1 = {
+  position: "bottom",
+  image_src: "images/pic02.jpg",
+  content: {
+    heading: "heading - lorem",
+    subheading: "subheading - lorem",
+    paragraphs: [],
+  },
+};
+
+const spotlight_2 = {
+  position: "right",
+  image_src: "images/pic03.jpg",
+  content: {
+    heading: "heading - lorem",
+    paragraphs: [],
+  },
+};
+
+const spotlight_3 = {
+  position: "left",
+  image_src: "images/pic04.jpg",
+  content: {
+    heading: "heading - lorem",
+    paragraphs: [],
+  },
+};
