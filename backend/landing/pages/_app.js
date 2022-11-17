c6b5bf6fe90d695fb6bdb406eeecbf6e02bc7358
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-17 15:09:34 +0200

    refactor: site entry files

--- backend/landing/pages/_app.js
+++ backend/landing/pages/_app.js
@@ -1,5 +1,5 @@
-import "@/styles/_03_base/base.scss";
-import "@/styles/_06_system/pages/index.page.scss";
+import "@/styles/base/base.scss";
+import "@/styles/pages/index.page.scss";
 
 import { useEffect } from "react";
 
