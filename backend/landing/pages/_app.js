c6b05355e85ad5bc266f85c98381cbef60b88ac7
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-17 15:11:56 +0200

    refactor: remove bootstrap import

--- backend/landing/pages/_app.js
+++ backend/landing/pages/_app.js
@@ -1,13 +1,7 @@
 import "@/styles/base/base.scss";
 import "@/styles/pages/index.page.scss";
 
-import { useEffect } from "react";
-
 function MyApp({ Component, pageProps }) {
-  // useEffect(() => {
-  //   require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  // }, []);
-
   return <Component {...pageProps} />;
 }
 
