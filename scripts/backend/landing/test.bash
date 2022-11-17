d5dccdd236b0a39f373286919cc11769110f95d6
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-17 14:34:18 +0200

    ci: adjust ci-cd-scripts

--- scripts/backend/landing/test.bash
+++ scripts/backend/landing/test.bash
@@ -2,5 +2,5 @@
 # makes sure the folder containing the script will be the root folder
 cd "$(dirname "$0")" || exit
 
-echo "LINT Next.js LynxIT Landing Page"
-npm --prefix ../../../backend/landing run lint
+echo "TEST Next.js LynxIT Landing Page"
+npm --prefix ../../../backend/landing run test
