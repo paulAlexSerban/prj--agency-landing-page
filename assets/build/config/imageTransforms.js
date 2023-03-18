a3497913edf58397f7d78e0e992b5d9242e74448
paulAlexSerban <paul.alex.serban@gmail.com>
2023-03-18 03:20:15 +0200

    style(formats): prettier

--- assets/build/config/imageTransforms.js
+++ assets/build/config/imageTransforms.js
@@ -4,63 +4,63 @@ const transforms = [
     options: {
       width: 480,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
   {
     options: {
       width: 640,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
   {
     options: {
       width: 960,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
   {
     options: {
       width: 1080,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
   {
     options: {
       width: 1280,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
   {
     options: {
       width: 1440,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
   {
     options: {
       width: 1920,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
   {
     options: {
       width: 2040,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
   {
     options: {
       width: 2880,
       fit: "cover",
-      quality:100
+      quality: 100,
     },
   },
 ];
